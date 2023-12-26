55cddfc68a260de7271abc562fe40cddd388ca9e
Luiz Teles <luizteles934@gmail.com>
2023-12-26 17:01:25 -0300

    feat: Repositório de autenticação

---
+++ src/repositories/authRepository.ts
@@ -0,0 +1,19 @@
+import prisma from '../database/database';
+import type { User, UserWithProfile } from './authRepostiroy.d';
+
+export default class AuthRepository {
+  public async getUserByEmail(
+    data: Omit<User, 'id' | 'username' | 'password'>,
+  ): Promise<User | null> {
+    return await prisma.user.findUnique({ where: { email: data.email } });
+  }
+
+  public async createUser(
+    data: Omit<UserWithProfile, 'id'>,
+  ): Promise<Omit<User, 'username' | 'email' | 'password'>> {
+    return await prisma.user.create({
+      data: { ...data },
+      select: { id: true },
+    });
+  }
+}
