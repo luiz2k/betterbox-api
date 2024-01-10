4b7c2fd8b21c396c661a43e300249dc2ebd9dc9f
Luiz Teles <luizteles934@gmail.com>
2024-01-10 15:54:40 -0300

    feat(authRoute.ts): Rota para deslogar e importação do middleware de autenticação

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import AuthController from '../controllers/authController';
+import UserAuthentication from '../middlewares/userAuthentication';
 
 export default class AuthRoute {
   public router: Router;
   private authController: AuthController;
 
+  private userAuthentication: UserAuthentication;
+
   constructor() {
     this.router = Router();
     this.authController = new AuthController();
+    this.userAuthentication = new UserAuthentication();
     this.routes();
   }
 
@@ -20,6 +24,13 @@ export default class AuthRoute {
     this.router.post(
       '/signup',
       this.authController.signUp.bind(this.authController),
+    );
+
+    this.router.use(this.userAuthentication.verifyAutentication);
+
+    this.router.post(
+      '/signout',
+      this.authController.signOut.bind(this.authController),
     );
   }
 }
