c2908026a5ede40b04ccb197794f8c1ad84376a8
Luiz Teles <luizteles934@gmail.com>
2024-01-14 17:41:42 -0300

    fix(authValidation.ts): Correção no esqueda de validação da criação de usuário

--- src/validations/authValidation.ts
+++ src/validations/authValidation.ts
@@ -3,12 +3,12 @@ import z from 'zod';
 export const signUpSchema = z.object({
   username: z
     .string()
-    .min(3, 'O usuário deve ter no minímo 3 caracteres.')
+    .min(2, 'O usuário deve ter no minímo 2 caracteres.')
     .max(15, 'O usuário deve ter no maximo 15 caracteres.'),
   email: z
     .string()
     .email('Informe um e-mail válido.')
-    .min(5, 'O e-mail deve ter no mínimo 7 caracteres.')
+    .min(5, 'O e-mail deve ter no mínimo 5 caracteres.')
     .max(254, 'O e-mail deve ter no máximo 254 caracteres.'),
   password: z
     .string()
